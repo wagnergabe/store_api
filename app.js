f0859679e7832a4fb7eb0f75e961804f9586011d
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-03 23:07:18 -0500

    add middleware errors/startup server

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+//middleware
 app.use(express.json())
 
+//routes
 app.get('/', (req,res) => {
     res.send('<h1>Store API</h1><a href="api/v1/products">products rout</a>')
 })
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000
+
+const start = async() => {
+    try{
+        app.listen(port, console.log(`Server is listening to port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+
+
+
