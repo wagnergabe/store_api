9fdcd0c8125d7691ef655812a5b4114e854abae4
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-04 16:57:28 -0500

    connected to db...i think

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
+
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -22,6 +24,7 @@ const port = process.env.PORT || 3000
 
 const start = async() => {
     try{
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening to port ${port}...`))
     } catch (error) {
         console.log(error)
