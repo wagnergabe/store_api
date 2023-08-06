6192d22902952b5b01a3a48e48714bbb6bff247f
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-05 21:37:49 -0500

    added controllers/router

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect')
-
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -14,8 +14,11 @@ app.use(express.json())
 
 //routes
 app.get('/', (req,res) => {
-    res.send('<h1>Store API</h1><a href="api/v1/products">products rout</a>')
+    res.send('<h1>Store API</h1><a href="api/v1/products">products route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
+
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
