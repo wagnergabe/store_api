bc8c4bd3bfb28334daa7fe8dab9d161728f70bca
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-08 23:24:47 -0500

    get all products working postman

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,12 @@
+const Product = require('../models/product')
+
+
+
 const getAllProductsStatic = async(req, res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({ msg: 'products testing route'})
+    const products = await Product.find({
+        featured: true
+     })
+    res.status(200).json({ products, numHits: products.length })
 }
 const getAllProducts= async(req, res) => {
     res.status(200).json({ msg: 'products route'})
