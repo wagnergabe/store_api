4295aebffdd74b1cf066070a8878297b926f171a
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-11 18:03:02 -0500

    added sort and select options

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, numHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -21,8 +21,24 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i'}
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+
+  let result = Product.find(queryObject)
+
+  //sort
+  if(sort){
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  }
+  else {
+    result = result.sort('createAt')
+  }
+
+if(fields) {
+    const fieldList = fields.split('').join(' ')
+    result = result.select(fieldList)
+}
+
+  const products = await result
   res.status(200).json({ products, numberHits: products.length });
 };
 
