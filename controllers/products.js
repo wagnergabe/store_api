2e4d9895f45735fc0a7d3f49ecdc2789a695ad72
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-10 23:03:35 -0500

    update query string parameters

--- controllers/products.js
+++ controllers/products.js
@@ -1,27 +1,32 @@
-const Product = require('../models/product')
+const Product = require("../models/product");
 
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    name: '2',
+  });
+  res.status(200).json({ products, numHits: products.length });
+};
+const getAllProducts = async (req, res) => {
+  const { featured, company, name } = req.query;
+  const queryObject = {};
 
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
 
-const getAllProductsStatic = async(req, res) => {
-    const products = await Product.find({
-        featured: true
-     })
-    res.status(200).json({ products, numHits: products.length })
-}
-const getAllProducts= async(req, res) => {
-    const { featured } = req.query
-const queryObject = {}
+  if (company) {
+    queryObject.company = company;
+  }
 
-if(featured) {
-    queryObject.featured = featured === 'true'? true : false
-}
-console.log(queryObject)
-    const products = await Product.find(queryObject)
-    res.status(200).json({ products, numberHits: products.length})
-}
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i'}
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, numberHits: products.length });
+};
 
 module.exports = {
-    getAllProducts,
-    getAllProductsStatic
-}
-
+  getAllProducts,
+  getAllProductsStatic,
+};
