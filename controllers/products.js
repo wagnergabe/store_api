06a7f0b0404bbd602b072fe3b85910d7801ef058
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-08-13 20:57:33 -0500

    update numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -26,7 +26,13 @@ const getAllProducts = async (req, res) => {
     }
     const regEx = /\b(<|>|>=|<|<=|=)\b/g
     let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
-    console.log(filters)
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item)=> {
+      const [field, operator, value] = item.split('-')
+      if(options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) } 
+      }
+    })
   }
 
   let result = Product.find(queryObject)
